8928597a1777d08aaa19776bec044bf8eb7d7cf8
Heshan Thenura Kariyawasam <heshanthenura@protonmail.com>
2023-09-29 00:01:30 +0530

    final changes

--- src/main/java/com/heshanthenura/fileshredder/Controllers/FileDetailsController.java
+++ src/main/java/com/heshanthenura/fileshredder/Controllers/FileDetailsController.java
@@ -1,9 +1,20 @@
 package com.heshanthenura.fileshredder.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+import java.io.File;
+import java.io.IOException;
+
+import static com.heshanthenura.fileshredder.Controllers.MainController.addedFilePaths;
+
 public class FileDetailsController {
+
+    @FXML
+    private VBox fileDetails;
+
     @FXML
     private Text fileName;
 
@@ -18,5 +29,61 @@ public class FileDetailsController {
         filePath.setText("Path: " + path);
         fileSize.setText("Size: " + size);
     }
+
+    @FXML
+    void removeFile(MouseEvent event) {
+        if (fileDetails != null) {
+            // Get the parent layout of fileDetailsHolder (assuming it's VBox)
+            VBox parentLayout = (VBox) fileDetails.getParent();
+
+            // Remove fileDetailsHolder from its parent layout
+            parentLayout.getChildren().remove(fileDetails);
+
+            // Remove the file path from the addedFilePaths list
+            String path = filePath.getText().substring("Path: ".length());
+            addedFilePaths.remove(path);
+            System.out.println("Updated file paths: " + addedFilePaths);
+        }
+    }
+
+
+
+
+    @FXML
+    void openLocation(MouseEvent event) {
+        // Get the file path from the filePath text field
+        String path = filePath.getText().substring("Path: ".length());
+
+        // Create a File object for the file
+        File file = new File(path);
+
+        // Check if the file exists and is not a directory
+        if (file.exists() && !file.isDirectory()) {
+            try {
+                // Get the parent directory of the file
+                File parentDir = file.getParentFile();
+
+                // Check if the parent directory exists
+                if (parentDir != null && parentDir.exists()) {
+                    String os = System.getProperty("os.name").toLowerCase();
+
+                    if (os.contains("mac")) {
+                        // macOS: Use "open" command
+                        Runtime.getRuntime().exec(new String[]{"open", "-R", parentDir.getAbsolutePath()});
+                    } else if (os.contains("nix") || os.contains("nux") || os.contains("unix")) {
+                        // Linux: Use "xdg-open" command
+                        Runtime.getRuntime().exec(new String[]{"xdg-open", parentDir.getAbsolutePath()});
+                    } else if (os.contains("win")) {
+                        // Windows: Use "explorer.exe" command
+                        Runtime.getRuntime().exec(new String[]{"explorer.exe", parentDir.getAbsolutePath()});
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 
 }
--- src/main/java/com/heshanthenura/fileshredder/Controllers/MainController.java
+++ src/main/java/com/heshanthenura/fileshredder/Controllers/MainController.java
@@ -3,22 +3,37 @@ package com.heshanthenura.fileshredder.Controllers;
 import com.heshanthenura.fileshredder.MainApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
 
 public class MainController {
 
+    public static List<String> addedFilePaths = new ArrayList<>();
+    private FileDetailsController fileDetailsController;
+
+
     Logger logger = Logger.getLogger("info-logger");
 
     @FXML
-    private AnchorPane fileDropPane;
+    private ScrollPane fileDropPane;
 
     @FXML
     private VBox fileDetailsHolder;
@@ -38,22 +53,9 @@ public class MainController {
             boolean success = false;
 
             if (dragboard.hasFiles()) {
-                // List and process the dropped files
-                dragboard.getFiles().forEach(file -> {
-                    logger.info("Dropped file: " + file.getAbsolutePath());
-                    // You can perform actions with the dropped files here
-
-                    // Load filedetails.fxml and add it to fileDetailsHolder
-                    try {
-                        FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource("filedetails.fxml"));
-                        AnchorPane fileDetails = loader.load();
-                        FileDetailsController controller = loader.getController();
-                        controller.setFileDetails(file.getName(), file.getAbsolutePath(), Long.toString(file.length()));
-                        fileDetailsHolder.getChildren().add(fileDetails);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
+                // List and process the dropped files and folders
+                List<File> filesAndFolders = dragboard.getFiles();
+                processFilesAndFolders(filesAndFolders);
 
                 success = true;
             }
@@ -62,5 +64,116 @@ public class MainController {
             event.consume();
         });
     }
+
+    private void processFilesAndFolders(List<File> filesAndFolders) {
+        for (File fileOrFolder : filesAndFolders) {
+            if (fileOrFolder.isDirectory()) {
+                // If it's a directory, recursively process its contents
+                processFilesAndFolders(List.of(fileOrFolder.listFiles()));
+            } else {
+                // If it's a file, process it
+                String filePath = fileOrFolder.getAbsolutePath();
+                logger.info("Dropped file: " + filePath);
+
+                // Check if the path is not already in the list
+                if (!addedFilePaths.contains(filePath)) {
+                    addedFilePaths.add(filePath);
+
+                    // Load filedetails.fxml and add it to fileDetailsHolder
+                    try {
+                        FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource("filedetails.fxml"));
+                        VBox fileDetails = loader.load();
+                        fileDetailsController  = loader.getController();
+                        fileDetailsController.setFileDetails(fileOrFolder.getName(), filePath, Long.toString(fileOrFolder.length()));
+                        fileDetailsHolder.getChildren().add(fileDetails);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        for (String path :
+                addedFilePaths) {
+            logger.info(path);
+        }
+
+    }
+
+    public void removeFilePath(String path) {
+        addedFilePaths.remove(path);
+    }
+
+    @FXML
+    void shredAndDeleteFiles(MouseEvent event) {
+        int passes = 1;
+
+        List<String> filesToDelete = new ArrayList<>(); // Store file paths to delete
+
+        for (String filePath : addedFilePaths) {
+            File file = new File(filePath);
+
+            if (file.exists() && !file.isDirectory()) {
+                try (FileOutputStream fos = new FileOutputStream(file, false)) {
+                    for (int i = 0; i < passes; i++) {
+                        // Overwrite the file's contents with pseudorandom data
+                        byte[] buffer = new byte[1024];
+                        new Random().nextBytes(buffer);
+                        fos.write(buffer);
+                    }
+                    logger.info("Shredding process completed for " + filePath);
+
+                    // Add the file path to the list of files to delete
+                    filesToDelete.add(filePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // Now, delete the files one by one
+        for (String filePath : filesToDelete) {
+            File fileToDelete = new File(filePath);
+            if (fileToDelete.exists() && fileToDelete.isFile()) {
+                if (fileToDelete.delete()) {
+                    logger.info("File shredded and deleted: " + filePath);
+                } else {
+                    logger.warning("Failed to delete file after shredding: " + filePath);
+                }
+            }
+        }
+
+        // Remove the associated child elements from fileDetailsHolder
+        removeFileDetailsHolder();
+    }
+
+
+    @FXML
+    void shredFiles(MouseEvent event) {
+        int passes = 1;
+
+        for (String filePath : addedFilePaths) {
+            File file = new File(filePath);
+
+            if (file.exists() && !file.isDirectory()) {
+                try (FileOutputStream fos = new FileOutputStream(file, false)) {
+                    for (int i = 0; i < passes; i++) {
+                        // Overwrite the file's contents with pseudorandom data
+                        byte[] buffer = new byte[1024];
+                        new Random().nextBytes(buffer);
+                        fos.write(buffer);
+                    }
+                    logger.info("Shredding process completed for " + filePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        removeFileDetailsHolder();
+    }
+
+    private void removeFileDetailsHolder() {
+        fileDetailsHolder.getChildren().clear();
+    }
+
 
 }
